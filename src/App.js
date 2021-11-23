1bfe5a7ceed68530d481bbcd7dcea9726de35f68
MariusDevelops <brazasmarius@gmail.com>
2021-11-23 11:40:32 +0100

    Basic component structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ImgEquilibrium from "./images/image-equilibrium.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Card />
+      <Title />
+      <Text />
+      <div>
+        <EthereumIcon />
+        <ClockIcon />
+      </div>
+      <Author />
+    </section>
   );
 }
+
+const Title = () => <h1>Equilibrium #3429</h1>;
+const Card = () => <img src={ImgEquilibrium} alt="card equilibrium" />;
+const Text = () => <p>Our Equilibrium collection promotes balance and calm.</p>;
+const EthereumIcon = () => {
+  return <span className="icon1">0.041 ETH</span>;
+};
+const ClockIcon = () => {
+  return <span className="icon2">3 days left</span>;
+};
+const Author = () => {
+  return <span className="image-avatar">Creation of Jules Wyvern</span>;
+};
 
 export default App;

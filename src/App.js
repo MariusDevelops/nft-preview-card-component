52928bf4f49b02582af8fd903fe5ac6477fc1bfb
MariusDevelops <brazasmarius@gmail.com>
2021-12-16 20:59:33 +0100

    Some small fixes

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import Icon3View from "./images/icon-view.svg";
 function App() {
   return (
     <main>
-    <section className="container">
-      <div className="container2">
-        <Card></Card>
-        <CardHover />
-      </div>
-      <Title />
-      <Text />
-      <div className="twoElements">
-        <EthereumIcon />
-        <ClockIcon />
-      </div>
-      <hr />
-      <Author />
-    </section>
-    <Footer />
+      <section className="container">
+        <div className="container2">
+          <Card></Card>
+          <CardHover />
+        </div>
+        <Title />
+        <Text />
+        <div className="twoElements">
+          <EthereumIcon />
+          <ClockIcon />
+        </div>
+        <hr />
+        <Author />
+      </section>
+      <Footer />
     </main>
   );
 }
@@ -64,11 +64,26 @@ const Author = () => {
 const Footer = () => {
   return (
     <div className="attribution">
-    <p>
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="https://mariusdevelops.github.io/" rel="noreferrer" target="_blank">Marius</a>.
-    </p>
-  </div>
+      <p>
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://mariusdevelops.github.io/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Marius
+        </a>
+        .
+      </p>
+    </div>
   );
 };
 
